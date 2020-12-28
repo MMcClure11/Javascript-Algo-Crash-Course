d5b519fcd8c7b632316c17678799b2e5f9b79701
MMcClure11 <mcclme02@luther.edu>
2020-12-28 09:41:12 -0600

    Initial commit

---
+++ instersection-of-two-arrays.js
@@ -0,0 +1,7 @@
+// find the common elements between two arrays
+
+// nums1 = [1,2,2,1]
+// nums2 = [2,2]
+// Output: [2]
+
+//The Set object lets you store unique values of any type, whether primitive values or object references.
