7d4cfb4726e302279671a7554998a8369e5fa41e
MMcClure11 <mcclme02@luther.edu>
2020-12-29 09:22:16 -0600

    Update jump-game to include examples and description

--- jump-game.js
+++ jump-game.js
@@ -1,4 +1,17 @@
+// Given an array of non-negative integers,
+// determine if a player can jump to the last index of the array
+// using the values of the indices as their potential jumping distance. 
 
+// Hint we will use a greedy algorithm and some maths to work backwards through the array
+
+//  0  1  2
+// [2, 0, 0]
+//set validIndex = 2 (last index)
+//check if index to left plus its value is greater than or equal to the current validIndex
+//if yes, make that index the new validIndex
+// 1 + 0 >= 2 ? false
+// 0 + 2 >= 2 ? true, validIndex = 0
+// if 0 is a validIndex, the whole function should return true
 
 const canJump = (nums) => {
   if(!nums){
