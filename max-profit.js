4dae426966babc81b081c163bff5a6a37d504620
MMcClure11 <mcclme02@luther.edu>
2020-12-30 11:39:58 -0600

    Add notes for max-profit

---
+++ max-profit.js
@@ -0,0 +1,10 @@
+//google question
+
+//given a function called rob(), 
+//need to determine maximum number of houses you can rob without alerting the police
+//can not rob houses next to eachother
+  //rob 1st and 3rd or 2nd and 4th 
+//[8,1,3,10,7]
+//make new array
+//[8,8,11,18, 18] need to return 18
+  //greedy algorithm
