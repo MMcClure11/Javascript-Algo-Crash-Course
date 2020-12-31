91f0d716c0d1f804fdb46ee408c6cdfc56e1991a
MMcClure11 <mcclme02@luther.edu>
2020-12-31 10:22:43 -0600

    Add notes to linked-list-cycle

---
+++ linked-list-cycle.js
@@ -0,0 +1,9 @@
+// common question asked by many big companies
+//You are given a function hasCycle()
+//which takes in a linked list called head
+//determine if that linked list is a cycle (return true or false)
+
+//consider a loop 1 -> 2-> 3-> 4-> 5-> (pointing back to 1)
+// if you have a slow(moves 1 node) and fast pointer(moves 2 nodes) 
+//check to see if they ever line up on the same node again
+
