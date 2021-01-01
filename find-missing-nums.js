3ead749675d1dfe3917c9907f7260814c42658e3
MMcClure11 <mcclme02@luther.edu>
2021-01-01 13:33:12 -0600

    Add solve for find-missing-nums

--- find-missing-nums.js
+++ find-missing-nums.js
@@ -13,3 +13,26 @@
 //I think how you solve really depends on if the given array is sequential or not, 
 //and if it has more than single digits…
 //Some prompts indicate there will only be one num missing.
+
+const findMissingNums = (nums) => {
+  for(let i = 0; i < nums.length; i++){
+    let q = Math.abs(nums[i]) - 1
+    nums[q] = Math.abs(nums[q]) * -1
+  }
+
+  let results = []
+
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] > 0){
+      results.push(i + 1)
+    }
+  }
+  return(results)
+}
+
+let arr1 = [1,2,4,2] //=> [3]
+let arr2 = [1,2,3,4,7,8] //=> [5,6]
+let arr3 = [4,3,2,7,8,2,3,1] //=> [5,6]
+let arr4 = [1,5,7,8,6] //=> [2,3,4]
+
+findMissingNums(arr5)
