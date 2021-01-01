5c60307499ad404f2e9ec2212d58ebd459943ad7
MMcClure11 <mcclme02@luther.edu>
2021-01-01 13:29:45 -0600

    Add instructions to find-missing-nums

---
+++ find-missing-nums.js
@@ -0,0 +1,7 @@
+// Given an array, find the numbers that are missing from the sequence
+// Examples: 
+// [4,3,2,7,8,2,3,1] //=> [5,6]
+// [1,2,3,4,7,8] //=> [5,6]
+// [1,2,4,2] //=> [3]
+// [1,5,7,8,6] //=> [2,3,4]
+// Solve in O(n)
