53e6d9e9a7cf5f7bf02a9e46aef2e279ef650c51
MMcClure11 <mcclme02@luther.edu>
2021-01-02 11:27:13 -0600

    Add solve for isPerfectSquare

---
+++ is-perfect-square.js
@@ -0,0 +1,25 @@
+const isPerfectSquare = (num) => {
+  if(num<1){
+    return false
+  }
+
+  let left = 1
+  let right = num
+
+  while(left <= right) {
+    let midpoint = left + Math.floor((right - left)/2)
+
+    if(midpoint * midpoint === num) {
+      return true
+    } else if (midpoint * midpoint > num) {
+      right = midpoint - 1
+    } else if (midpoint * midpoint < num) {
+      left = midpoint + 1
+    }
+  }
+  return false
+}
+
+// isPerfectSquare(9) // true
+// isPerfectSquare(25) // true
+isPerfectSquare(8) //false
