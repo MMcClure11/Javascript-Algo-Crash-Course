80acd7bd64f281aae466e234798ec85ec26daec0
MMcClure11 <mcclme02@luther.edu>
2021-01-04 09:13:18 -0600

    Add notes and instructions to max-sum

---
+++ max-sum.js
@@ -0,0 +1,11 @@
+// Given an array return the greatest possible sum of two of its numbers
+
+// [-1,2,3,-5,4]
+// need two vars
+// current = -1,2,5,0,4
+// is the current greater or current plus the maxCurrent ? 
+// 2 + -1 === 1; which is greater 1 or 2 ? 2! so update current with 2
+// global = -1,2,5
+// which is greater -1 or 2 ? 2, update global with two
+
+// add first element to both vars
