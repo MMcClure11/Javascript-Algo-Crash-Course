36134e9fdd3510ac808cc8e62fefed567f1ed14a
MMcClure11 <mcclme02@luther.edu>
2021-01-05 09:48:40 -0600

    Add Shan solve for binary-tree-symetric

--- binary-tree-symmetric.js
+++ binary-tree-symmetric.js
@@ -3,3 +3,25 @@
 //return true if it a mirror of itself and false if it is not
 
 //Recursion: a method of solving a problem where the solution depends on small instances of the same problem
+
+const isSymmetric = (root) => {
+
+  if(root === null) {
+    return true
+  }
+
+  return isMirror(root.left, root.right)
+}
+
+const isMirror = (tree1, tree2) => {
+
+  if(tree1 === null || tree2 === null) {
+    return tree1 === tree2
+  }
+
+  if(tree1.val !== tree2.val) {
+    return false
+  }
+
+  return isMirror(tree1.left, tree2.right) && isMirror(tree1.right, tree2.left)
+}
