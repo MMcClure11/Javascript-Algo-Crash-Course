74ee23120b780e23d3775be8ee42ac5c70e03316
MMcClure11 <mcclme02@luther.edu>
2021-01-05 09:30:34 -0600

    Add binary tree file with explantion

---
+++ binary-tree-symmetric.js
@@ -0,0 +1,5 @@
+//Amazon Algo problem
+//Given a binary tree, and a function isTreeSymmetric
+//return true if it a mirror of itself and false if it is not
+
+//Recursion: a method of solving a problem where the solution depends on small instances of the same problem
