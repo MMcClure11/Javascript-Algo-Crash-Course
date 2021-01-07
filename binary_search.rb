c317baa60ba105763a2b1248289705eceaa7ea83
MMcClure11 <mcclme02@luther.edu>
2021-01-07 13:04:24 -0600

    Add notes about iterative binary search

--- binary_search.rb
+++ binary_search.rb
@@ -3,6 +3,9 @@
 #specify an arr_size so we can reuse it
 #To run this file: ruby binary_search.rb
 
+#Two ways to write out the O(lg(n)) binary search algorithm, iteratively and recursively
+#the iterative solution uses less space than the recursive version
+
 #ITERATIVE BINARY SEARCH, continuously divide problem in half is how we get that O(lg(n))
 def binary_search_iter(arr, el)
   max = arr.length - 1
@@ -21,6 +24,11 @@ def binary_search_iter(arr, el)
 
   return nil
 end
+#return nil if not found, return for when we do find it, statements that change the conditions of the loop
+#to push it towards breaking the loop
+#to make sure we are not in an infinite loop, each iteration lowers max or raises min value which pushes us closer to breaking the loop
+#eventually will break the loop and return nil or return the mid value
+
 
 arr_size = 1000
 r = Random.new
@@ -34,7 +42,7 @@ search_num = r.rand(arr_size)
 #so running it multiple times we can see worst and non-worst case scenarios
 
 puts "Looking for #{search_num}"
-
+puts
 #LINEAR INDEX SEARCH
 #Ruby index method O(n) linear search, returns nil if cannot find that num, or the index it was found at
 puts "Ruby #index O(n)"
@@ -42,6 +50,15 @@ resi = search_arr.index(search_num)
 puts resi.nil? ? "Could not find #{search_num}" : "Found #{search_arr[resi]} at index #{resi}"
 #want to make sure that we found the right number for when we right our own methods
 
+puts
 
-#Two ways to write out the O(lg(n)) binary search algorithm, iteratively and recursively
-#the iterative solution uses less space than the recursive version
+puts "Binary Search Iterative O(lg(n))"
+resbi = binary_search_iter(search_arr, search_num)
+puts resbi.nil? ? "Could not find #{search_num}" : "Found #{search_arr[resbi]} at index #{resbi}"
+#Note: we may not find the number at the same index every single time
+#because there may be duplicates of the search_num in the search_array (Side effect of using a random number generator to populate it)
+#AND because of the two different ways that linear vs binary search through the array
+#linear searches left to right, and binary doesn't, you might find the correct number at a different index
+#it is acceptable because it is returning the correct solution
+#because both found A valid index with that number in it
+
