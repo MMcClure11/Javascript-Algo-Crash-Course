6bb7d1f805a2ae88236b94456fe2e31b737f1d5b
MMcClure11 <mcclme02@luther.edu>
2021-01-07 10:57:42 -0600

    WIP binary_search iterative

--- binary_search.rb
+++ binary_search.rb
@@ -3,6 +3,21 @@
 #specify an arr_size so we can reuse it
 #To run this file: ruby binary_search.rb
 
+#ITERATIVE BINARY SEARCH, continuously divide problem in half is how we get that O(lg(n))
+def binary_search_iter(arr, el)
+  max = arr.length - 1
+  min = 0
+
+  while min <= max #know that if the size difference between the min and the max is 0 or negative, we've gone through the whole array and did not find the element
+    mid = (min + max) / 2
+    if arr[mid] == el
+      return mid
+    end
+  end
+
+  return nil
+end
+
 arr_size = 1000
 r = Random.new
 
@@ -16,8 +31,13 @@ search_num = r.rand(arr_size)
 
 puts "Looking for #{search_num}"
 
+#LINEAR INDEX SEARCH
 #Ruby index method O(n) linear search, returns nil if cannot find that num, or the index it was found at
 puts "Ruby #index O(n)"
 resi = search_arr.index(search_num)
 puts resi.nil? ? "Could not find #{search_num}" : "Found #{search_arr[resi]} at index #{resi}"
 #want to make sure that we found the right number for when we right our own methods
+
+
+#Two ways to write out the O(lg(n)) binary search algorithm, iteratively and recursively
+#the iterative solution uses less space than the recursive version
