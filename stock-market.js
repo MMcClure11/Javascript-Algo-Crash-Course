c4acb541968d1412b9213d144670a91f57d4e6eb
MMcClure11 <mcclme02@luther.edu>
2021-01-06 18:12:41 -0600

    Add notes for stock-market

---
+++ stock-market.js
@@ -0,0 +1,13 @@
+//Amazon
+
+//When is the best time to buy and sell stock
+//takes in array of numbers
+
+//[7,1,5,3,6,4]
+// minBuyPrice = 1
+//maxProfit = 6-1 = 5
+//must buy stock first then sell it
+//need to vars
+//minBuyPrice and maxProfit
+
+
