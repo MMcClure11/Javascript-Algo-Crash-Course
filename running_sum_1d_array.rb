a08170a2639cfef33fcd162033a73ced77cd822d
MMcClure11 <mcclme02@luther.edu>
2021-01-13 13:26:21 -0600

    Add instructions and expected outputs

---
+++ running_sum_1d_array.rb
@@ -0,0 +1,10 @@
+#Given an array of nums, we define a running sum of an array as 
+#runningSum[i] = sum(nums[0]...nums[i])
+#return the running sum of nums
+
+#input nums = [1,2,3,4]
+#output [1,3,6,10]
+
+#input nums=[1,1,1,1,1,1]
+#output [1,2,3,4,5]
+
