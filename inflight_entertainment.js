64be7e1cc255915a6b06d8ad9444005be844ec5f
MMcClure11 <mcclme02@luther.edu>
2021-01-21 09:35:28 -0600

    Add notes for inflight entertainment

--- inflight_entertainment.js
+++ inflight_entertainment.js
@@ -6,14 +6,20 @@
 // don't make users watch same movie twice
 // optimize for runtime over memory ( O(n)) where n is the length of movies
 
+//https://drive.google.com/file/d/1UXkk06Im3-m2G4dHk0Q2nJ1yYiJrwS9v/view 
+// source was Interview Cake (SKILLED USES THIS ONE OR SOMETHING VERY SIMILAR IN THEIR JS ASSESSMENT)
+
+//Know two movie lengths need to sum  flightLength
+// given firstMovieLength we need flightLength - firstMovieLength = secondMovieLength
 const canTwoMoviesFillFlight = (movieLengths, flightLength) => {
-  const movieLengthsSeen = new Set()
+  const movieLengthsSeen = new Set() //movieLengths we've seen so far
 
   for(let m of movieLengths) {
+
     if(movieLengthsSeen.has(flightLength - m)) {
       return true
     }
     movieLengthsSeen.add(m)
   }
-  return false 
+  return false //we never found a match so return false 
 }
