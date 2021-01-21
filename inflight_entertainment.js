3e78f313686ea29fcc11028f2774d05a71b3f113
MMcClure11 <mcclme02@luther.edu>
2021-01-21 09:07:31 -0600

    Add instructions for inflight entertainment

---
+++ inflight_entertainment.js
@@ -0,0 +1,7 @@
+// Build a feature for choosing two movies whose total runtimes will equal the exact flight length
+// Write a fxn that takes an integer flightLength and an array of integers movieLengths (both in minutes)
+// return a boolean indicating whether there are two nums in movieLengths whose sum equals flightLength
+
+// assume watch exactly 2 movies
+// don't make users watch same movie twice
+// optimize for runtime over memory ( O(n)) where n is the length of movies
