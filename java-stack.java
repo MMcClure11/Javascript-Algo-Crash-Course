9599bdd1fe4c3f653cf3f6d83eaf76e16de95304
MMcClure11 <mcclme02@luther.edu>
2021-03-12 11:56:40 -0600

    Add example 1 and function description with constraints to java-stack

--- java-stack.java
+++ java-stack.java
@@ -15,8 +15,36 @@ true if the string is balanced, or false if it is not.
 Example 0 
 
 s = ["{}()", "{()}","({()})"]
-s[0] exhibits condition 2 above. "{}" and "()" are balanced so "{}()"is balanced. return true
-s[1] exhibits condition 3 above. "()" is balanced, so "{()}" is balanced. return true
-s[2] exhibits condition 3 above. so "({())})" is balanced. return true
+s[0] exhibits condition 2 above. "{}" and "()" are balanced so "{}()"is balanced. Return true.
+s[1] exhibits condition 3 above. "()" is balanced, so "{()}" is balanced. Return true.
+s[2] exhibits condition 3 above. so "({())})" is balanced. Return true.
+
+Example 1 
+
+s = ["{}(", "({)}", "((", "}{"]
+s[0] rarr 2. "{}(" is an unbalanced string due to the open "(". Return false. 
+s[1] rarr 2. "({)}" is an unbalanced string due to ")" before "{" has been closed. Return false.
+s[2] rarr 2. "((" is an unbalanced string because neither "(" is closed. Return false.
+s[3] rarr 2. "}{" is an unbalanced string because "}" is not closed. Return false. 
+
+Function Description
+The provided code contains the declaration for a class named Solution with a main method that does the following:
+ - creates a Parser Object
+ - Reads an unknown number of strings from stdin
+ - Passes each string as an argument to the Parser object's isBalanced method and prints value 
+  returned by the method on a new line
+
+Complete the function isBalanced in the editor below. 
+
+isBalanced has the following parameters:
+  string s: a string of characters to check for balance
+  Returns: 
+    bool: a boolean that denotes whether the string is balanced:
+      true if the string is balanced
+      false if it is not
+
+Constraints
+  - Each string consist only of the characters {, }, (, )
+  - Each string has fewer than 50 characters.
 */
 
