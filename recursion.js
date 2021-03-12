e9d9bb38583dcac8cf829394fe6093b77f708535
MMcClure11 <mcclme02@luther.edu>
2021-03-12 16:20:17 -0600

    Add instructions for recursive countdown

--- recursion.js
+++ recursion.js
@@ -9,4 +9,14 @@ function countup(n) {
     return countArray;
   }
 }
-console.log(countup(5));
+console.log(countup(5)); //[ 1, 2, 3, 4, 5 ]
+
+/* We have defined a function called countdown with one parameter (n).
+ The function should use recursion to return an array 
+ containing the integers n through 1 based on the n parameter. 
+ If the function is called with a number less than 1, 
+ the function should return an empty array. 
+ For example, calling this function with n = 5 should 
+ return the array [5, 4, 3, 2, 1]. 
+*/
+
