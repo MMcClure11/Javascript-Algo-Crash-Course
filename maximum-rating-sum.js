c9c8b52729ce44ca3dae00c9b8adc4a7dc5d32d8
MMcClure11 <mcclme02@luther.edu>
2021-03-12 11:23:03 -0600

    Add description for maximum-rating-sum

---
+++ maximum-rating-sum.js
@@ -0,0 +1,19 @@
+/*
+3/11/2021 Part 2/2 Confluent HackerRank challenge, allowed to pick which programming language
+
+Maximum Rating Sum
+Ryan is movie obsessed and has a collected list of movie quality ratings. 
+He wants to watch the largest contiguous list of movies with teh highest cumulative ratings possible. 
+To do this, he must calculate the sum of all contiguous subarrays in order to determine the maximum possible subarray sum.
+
+For example, ratings are arr=[-1,3,4.-2,5,-7]
+We can see that the highest contiguous subarray runs from arr[1]-arr[4] 
+  and is 3 + 4 + -2 + 5 = 10 
+
+Function Description
+Complete the function maximumSum in the editor below. 
+It must return a long integer denoting the maximum sum for any contiguous subarray in arr.
+
+maximumSum has the following parameters:
+arr[arr[0],...arr[n-1]]: an array of integers
+*/
