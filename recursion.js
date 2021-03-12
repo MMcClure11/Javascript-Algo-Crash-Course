b14ed1464353ed10aff8ae20eaa1a1d916545797
MMcClure11 <mcclme02@luther.edu>
2021-03-12 16:27:05 -0600

    Add descriptions for rangeOfNumbers with recursion

--- recursion.js
+++ recursion.js
@@ -1,3 +1,4 @@
+// freecodecamp basic javascript
 //Use recursion to create a countup
 
 function countup(n) {
@@ -30,3 +31,13 @@ function countdown(n) {
   }
 }
 console.log("countdown", countdown(5));
+
+/*
+We have defined a function named rangeOfNumbers with two parameters. 
+The function should return an array of integers which begins with a number 
+represented by the startNum parameter and ends with a number represented 
+by the endNum parameter. The starting number will always be less than or 
+equal to the ending number. 
+Your function must use recursion by calling itself and not use loops of any kind. 
+It should also work for cases where both startNum and endNum are the same.
+*/
