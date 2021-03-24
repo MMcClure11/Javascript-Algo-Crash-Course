9e127eef8ad59fc6c22d793facdfd2a2374bf286
MMcClure11 <mcclme02@luther.edu>
2021-03-24 14:17:45 -0500

    Add instructions for newAperio challenge part 1

---
+++ newAperio.js
@@ -0,0 +1,5 @@
+//Live Code Challenge completed with Nick 3/24/21, given choice of language and platform
+//opted for javascript and codeSandBox so he could type too
+
+//1. Sort a string so that it is alphabetical
+
