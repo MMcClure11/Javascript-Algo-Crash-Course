cb79c229272ff32bd07fa104782742ec6fcd7309
MMcClure11 <mcclme02@luther.edu>
2021-03-24 14:22:58 -0500

    Add instructions for part 2

--- newAperio.js
+++ newAperio.js
@@ -1,10 +1,19 @@
-//Live Code Challenge completed with Nick 3/24/21, given choice of language and platform
-//opted for javascript and codeSandBox so he could type too
+/*
+Live Code Challenge completed with Nick 3/24/21, given choice of language and platform
+opted for javascript and codeSandBox so he could type too
 
-//1. Sort a string so that it is alphabetical
+1. Return a string so that it is sorted alphabetically
+assumptions: all lowercase and no special characters
+*/
 
 const alphabeticalSort = (s) => {
   return s.split('').sort().join('')
 }
 
 console.log(alphabeticalSort("cbsgdafer"))//abcdefgrs
+
+/*
+2. Now your function takes in a second argument with a custom alphabet.
+  same assumptions apply, all lowercase and no special characters, the alphabet does not repeat characters
+*/
+
