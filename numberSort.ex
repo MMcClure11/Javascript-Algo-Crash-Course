99e3c4d5501ae9cb4d60460d7e88805a54408619
MMcClure11 <mcclme02@luther.edu>
2021-04-03 14:56:54 -0500

    Add instructions for part two

--- numberSort.ex
+++ numberSort.ex
@@ -1,6 +1,9 @@
 # Part 1. Given an integer return the largest possible integer made out of its digits
 # assume positive numbers only and 0 is not included
 
+# Part 2. Make the function take in a second optional argument that is a list of digits 
+# in the order for the integer to be sorted in. 
+
 defmodule NumberSort do
   def numberSort(num) do 
     num
