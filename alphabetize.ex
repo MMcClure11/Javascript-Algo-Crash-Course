af2b6a2f1bbf7ad03a22a2d7dbd3133f93ab7415
MMcClure11 <mcclme02@luther.edu>
2021-04-03 17:47:01 -0500

    Add documentation for alphabetixze.ex

--- alphabetize.ex
+++ alphabetize.ex
@@ -52,17 +52,34 @@
 
 
 defmodule Alphabetize do
- 
+  @moduledoc """
+  Documentation for `Alphabetize`.
+  """
+
   @default_alphabet ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 
+  @doc """
+  Alphabetizes the given `string`. A custom `alphabet` may be provided, which
+  will be used for sorting.
+  
+  ## Examples
+  
+      iex> Alphabetize.alphabetize("newaperio")
+      "aeeinoprw"
+      
+      iex> alphabet = ~w(n b c d e f g h i j k l m a o p q r s t u v w x y z)
+      ...> Alphabetize.alphabetize("newaperio", alphabet)
+      "neeiaoprw"
+      
+  """
   def alphabetize(string, alphabet \\ @default_alphabet) do
     string
-    |> String.split("", trim: true)
-    |> Enum.sort(fn (a, b) -> compare_alpha?(a, b, alphabet) end)
+    |> String.split("")
+    |> Enum.sort(&compare_alpha(&1, &2, alphabet))
     |> Enum.join()
   end
 
-  defp compare_alpha?(a, b, alphabet) do
+  defp compare_alpha(a, b, alphabet) do
     Enum.find_index(alphabet, &(&1 == a)) < Enum.find_index(alphabet, &(&1 == b))
   end
 end
