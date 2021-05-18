415a02b825a2b7fe28472a356d8c8ed9fbb960ef
Megan McClure <mmcclure0100@gmail.com>
2021-05-18 08:47:52 -0500

    Add leetcode problem and solution to reverseInt

--- reverseInt.js
+++ reverseInt.js
@@ -7,3 +7,31 @@ reverseInt(23) //32
 reverseInt(280) //82
 reverseInt(-34) //-43
 reverseInt(0) //0
+
+/*
+Leetcode 
+7. Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+*/
+
+const reverseInt = x => {
+    const isNegative = x < 0;
+    const xStrArr = Math.abs(x).toString().split("");
+    const reversStr = xStrArr.reverse().join("");
+    const num = Number(reversStr);
+    if (isNegative && num > Math.pow(2, 31)) {
+        return 0;
+    }
+    if (!isNegative && num > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+    return isNegative ? -num : num;
+}
+
+reverseInt(23) //32
+reverseInt(280) //82
+reverseInt(-34) //-43
+reverseInt(0) //0
+reverseInt(124685142348566541685468524845) //0
