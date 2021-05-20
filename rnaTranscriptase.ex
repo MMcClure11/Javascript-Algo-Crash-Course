354ceb7c0e38a95317d690cb42194a2de06d0b23
Megan McClure <mmcclure0100@gmail.com>
2021-05-20 11:02:43 -0500

    Add instructions for RNATranscriptase

---
+++ rnaTranscriptase.ex
@@ -0,0 +1,18 @@
+# https://exercism.io/tracks/elixir/exercises/rna-transcription/solutions/774c49b5d0a74120b85cc854e8ef96d5
+
+# Exercism - Easy
+
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+
+# Both DNA and RNA strands are a sequence of nucleotides.
+
+# The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
+
+# The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G) and uracil (U).
+
+# Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
+
+# G -> C
+# C -> G
+# T -> A
+# A -> U
