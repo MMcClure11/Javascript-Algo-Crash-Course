d99963d1f1c01f8599b843e1561fe0b5efe4907f
Meks McClure <mmcclure0100@gmail.com>
2022-01-21 17:39:00 -0600

    Palindrome, jan 21, 2022 remembered every, but saw a peek of it before deletion

--- palindrome.js
+++ palindrome.js
@@ -10,7 +10,7 @@
 // 1/11/22
 // Direct solution
 function palindrome(str) {
-  return str === str.split("").reverse().join("");
+  return str.split("").reverse().join("") === str;
 }
 
 console.log(palindrome("meks")); // false
